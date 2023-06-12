27e4503b082ddf465fa85627a1e82545fe781cf6
aharris95 <105005248+aharris95@users.noreply.github.com>
2023-06-12 14:00:09 +0100

    add vite config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8080,
-    host: true,
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
   },
 });
